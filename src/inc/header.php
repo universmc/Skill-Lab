8c7b7e3990cbb33ba5eaba1745c43160c80daff7
universmc <hubmaster74@gmail.com>
2023-12-12 13:22:16 +0100

    interface

---
+++ src/inc/header.php
@@ -0,0 +1,69 @@
+<header>
+    <nav class="navbar">
+        <!-- Icône -->
+        <div class="navbar-icon">
+            <img src="path_to_your_icon.png" alt="Logo"><a href="pages/admin/admin.html">[-¡_]</a>
+        </div>
+    
+            <!-- Menu déroulant des cours -->
+            <div class="dropdown">
+                <button class="dropbtn"><a href="pages/cours/cours.html">Cours</a></button>
+                <div class="dropdown-content">
+                    <a href="#">Language</a>
+                    <div class="sub-dropdown">
+                        <a href="pages/cours/languages/html/index.html">HTML</a>
+                        <a href="pages/cours/languages/css/index.html">CSS</a>
+                        <a href="pages/cours/languages/sass/index.html">SASS</a>
+                        <a href="pages/cours/languages/ascii/index.html">ascii</a>
+                        <a href="pages/cours/languages/javascript/index.html">JavaScript</a>
+                        <a href="pages/cours/languages/python/index.html">python</a>
+                        <a href="pages/cours/languages/sql/index.html">sql</a>
+                        <a href="pages/cours/languages/gpt/index.html">gpt</a>
+                        <a href="#">React</a>
+                        <!-- Autres langages front-end -->
+                    </div>
+                </div>
+            </div>
+
+            <!-- Menu des projets -->
+            <div class="dropdown">
+                <button class="dropbtn"><a href="pages/projets/index.html">Projets</a></button>
+                <div class="dropdown-content">
+                    <a href="#">Projet 1</a>
+                    <a href="pages/projets/P3/index.html">Projet3</a>
+                    <a href="pages/projets/P3/index.html">Projet3</a>
+                    <a href="pages/projets/P4/index.html">Projet4</a>
+                    <!-- Liste des 12 projets -->
+                </div>
+            </div>
+    
+            <!-- Menu des soutenances -->
+            <div class="dropdown">
+                <button class="dropbtn">Soutenances</button>
+                <div class="dropdown-content">
+                    <a href="pages/soutenances/sessions/S1/index.html">Session 1</a>
+                    <a href="pages/soutenances/sessions/S2/index.html">Session 2</a>
+                    <a href="pages/soutenances/sessions/S3/index.html">Session 3</a>
+                    <!-- Autres sessions de soutenance -->
+                </div>
+            </div>
+        
+                    <!-- notes lab skill -->
+                    <div class="dropdown">
+                        <button class="dropbtn">Skill-lab</button>
+                        <div class="dropdown-content">
+                            <a href="#">Session 1</a>
+                            <!-- Autres sessions de soutenance -->
+                        </div>
+                    </div>
+
+            <!-- Menu des crédits -->
+            <div class="dropdown">
+                <button class="dropbtn">Crédits</button>
+                <div class="dropdown-content">
+                    <a href="#">About Portfolio</a>
+                </div>
+            </div>
+    </nav>
+    <?php include 'hero-ban.php'; ?> <!-- Inclusion de l'en-tête -->
+    </header> 
---
+++ src/inc/hero-ban.php
@@ -0,0 +1,15 @@
+<div class="hero-ban">
+    <div class="coln-h">
+            <div class="coln-H">H</div>
+            <div class="coln-H">H</div>
+            <div class="coln-H">H</div>
+            <div class="coln-H">H</div>
+            <div class="coln-H">H</div>
+    </div>
+    <div class="row-W">
+        <div class="inline">[]=== [] ====</div>
+        <div class="inline">=== [] ====</div>
+        <div class="inline">=== [] ====</div>
+        <div class="inline">=== [] ====[]</div>
+    </div>
+</div>
